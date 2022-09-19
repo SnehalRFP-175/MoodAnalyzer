e3520b0d660ca3b59c5722c16168ee3b70f1fb46
Snehal <nanavaresnehal115@gmail.com>
2022-09-19 07:01:21 +0530

    ADDED[Snehal] Refactor Tc1.2 Constructor Should Return Happy

--- src/main/java/com/MoodAnalyzer/Bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/MoodAnalyzer/Bridgelabz/MoodAnalyzer.java
@@ -3,14 +3,12 @@ package com.MoodAnalyzer.Bridgelabz;
 public class MoodAnalyzer
 {
     private String message;
-
     public MoodAnalyzer(String message) {
         this.message = message;
     }
-
     public String analyzeMood()
     {
-        if (message.contains("I am in Sad Mood"))
+        if (message.contains("I am in Happy Mood"))
             return "SAD";
         else
             return "HAPPY";
